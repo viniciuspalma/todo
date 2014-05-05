d5a7a1fc020d02caed93460520647fb6adb34b7d
viniciuspalma <vini_palma@outlook.com>
2014-05-05 17:47:48 -0300

    update responseto

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,11 @@
 class TodoController < ApplicationController
   def index
+    @todos = Todo.all
+
+    respond_to do |format|
+      format.html
+      format.xml {render xml: @todos}
+      format.json {render json: @todos}
+    end
   end
 end
