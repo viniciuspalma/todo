ed79c8a96576e21a41d5ee825b173d2e4f8a73c3
viniciuspalma <vini_palma@outlook.com>
2014-05-09 17:54:08 -0300

    add actions on controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,10 +1,41 @@
 class TodoController < ApplicationController
-  @todos = Todo.all
+  before_filter :get_all_todos
+
   def index
     render html: @todos
   end
 
   def all
     render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  def create
+    @todo = Todo.new
+    #@todo.id = params[:id]
+    @todo.title = params[:todo][:title]
+    @todo.active = params[:todo][:active]
+    if @todo.save
+      render text: 'Go'
+    else
+      render text: 'fail'
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo.destroy
+      render nothing: true
+    else
+      render text: 'Go'
+    end
+  end
+
+  def get_all_todos
+    @todos = Todo.all
   end
 end
