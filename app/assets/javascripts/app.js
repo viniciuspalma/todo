bf85450265ea31f7f0fa6aa188a9dbe32affe98f
viniciuspalma <vini_palma@outlook.com>
2014-05-09 17:47:17 -0300

    Remove fixtures

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,1 @@
-//= require jquery-1.10.2.min
-//= require handlebars-1.0.0
-//= require ember
-//= require ember-data
-
 window.Todos = Ember.Application.create();
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
-
-//= require_tree .
