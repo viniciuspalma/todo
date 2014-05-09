15cb3b7a7338f15e29a6dfaf1d75e9d564bab378
viniciuspalma <vini_palma@outlook.com>
2014-05-09 17:49:34 -0300

    update route

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,11 +1,9 @@
 Todos.Router.map(function() {
-  this.resource('todos', {path: '/'}, function() {
-    this.route('delete');
-  });
+  this.resource('todos', { path: '/' });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('todo');
+    return $.getJSON('/todo/all');
   }
 });
