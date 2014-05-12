7724b76b52433d6c7aa2188be394e888f8d90899
viniciuspalma <vini_palma@outlook.com>
2014-05-11 21:57:26 -0300

    resolve routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TodoList::Application.routes.draw do
   root :to => 'todo#index'
 
   post '/todos' => 'todo#create'
-  #get '/:id' => 'todo#show'
+  get 'todos' => 'todo#all'
   put '/todos/:id' => 'todo#update'
   delete '/todos/:id' => 'todo#delete'
 
