4c435e29eaa2fad908a8784bbbddcedf9b9dc78c
viniciuspalma <vini_palma@outlook.com>
2014-05-13 22:24:42 -0300

    cleanup frameworks

--- app/assets/javascripts/models/todo.js
+++
@@ -1,4 +0,0 @@
-Todos.Todo = DS.Model.extend({
-  title: DS.attr('string'),
-  active: DS.attr('boolean')
-});
--- app/assets/javascripts/store.js
+++
@@ -1,7 +0,0 @@
-Todos.Store = DS.Store.extend({
-  revision: 12,
-  adapter: DS.RESTAdapter.create({
-    url: 'http://localhost:3000',
-    bulkCommit: false
-  })
-});
