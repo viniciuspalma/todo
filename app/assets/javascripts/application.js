55d5fe637e0b8daa1f7e6c00b1b83d2fd30443cc
viniciuspalma <vini_palma@outlook.com>
2014-05-14 15:24:36 -0300

    add storage file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,64 +1,10 @@
 //= require jquery-1.10.2.min
+//= require handlebars-v1.3.0
+//= require storage
 
-(function (window) {
 
-  var defaults = {
-    'host': 'http://localhost:3000',
-    'namespace': '',
-    'resource': ''
-  };
+var store = new Storage({
+  resource: 'todos',
+  template: '#todos'
+});
 
-  function customizeOptions(options) {
-    var prop;
-    for (prop in defaults) {
-      if (!options.hasOwnProperty(prop)) {
-        options[prop] = defaults[prop];
-      }
-    }
-    return options;
-  }
-
-  var Storage = function Storage(options, data) {
-    this.options = customizeOptions(options || {});
-
-    this.host = this.options.host;
-    this.namespace = this.options.namespace;
-    this.resource = this.options.resource;
-
-    this.url = this.host + this.namespace + this.resource;
-
-    this.data = data || [];
-
-    this.populate();
-  }
-
-  Storage.prototype.populate = function() {
-    console.log(this.url)
-    $.ajax({
-      type: "GET",
-      url: this.url
-    })
-    .done(function( res ) {
-      console.log( res );
-    });
-
-    return this;
-  };
-
-  Storage.prototype.save = function(data) {
-    console.log(this.url)
-    $.ajax({
-      type: "POST",
-      url: this.url,
-      data: data
-    })
-    .done(function( res ) {
-      console.log( res );
-    });
-
-    return this;
-  };
-
-  window.Storage = Storage;
-
-}(this));
---
+++ app/assets/javascripts/storage.js
@@ -0,0 +1,82 @@
+(function (window) {
+
+  var defaults = {
+    'host': 'http://localhost:3000',
+    'namespace': '',
+    'resource': '',
+    'template': ''
+  };
+
+  function customizeOptions(options) {
+    var prop;
+    for (prop in defaults) {
+      if (!options.hasOwnProperty(prop)) {
+        options[prop] = defaults[prop];
+      }
+    }
+    return options;
+  }
+
+  function Storage(options, data) {
+    this.options = customizeOptions(options || {});
+
+    this.host = this.options.host;
+    this.namespace = this.options.namespace;
+    this.resource = this.options.resource;
+    this.template = this.options.template;
+
+    this.url = this.host + this.namespace + "/" + this.resource;
+
+    this.data = data || {};
+    this.data[this.resource] = [];
+
+    this.populate();
+    this.initializeView();
+
+  };
+
+  Storage.prototype.populate = function() {
+    var that = this;
+
+    $.ajax({
+      type: "GET",
+      url: this.url
+    })
+    .done(function(res) {
+      that.initializeView(res);
+    });
+
+    return this;
+  };
+
+  Storage.prototype.save = function(data) {
+
+    $.ajax({
+      type: "POST",
+      url: this.url,
+      data: data
+    })
+    .done(function( res ) {
+      console.log( res );
+    });
+
+    return this;
+  };
+
+  Storage.prototype.initializeView = function(res) {
+
+    var source = $(this.template).html()
+      , template = Handlebars.compile(source);
+
+    for(var i=0; i<res.length; i++) {
+        this.data[this.resource].push(res[i]);
+    }
+
+    console.log(this.data)
+    $("body").html(template(this.data));
+    return this;
+  };
+
+  window.Storage = Storage;
+
+}(this));
