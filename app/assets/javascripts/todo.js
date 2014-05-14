36102997bb5ac9f7687bc4f192d8047a94e58974
viniciuspalma <vini_palma@outlook.com>
2014-05-14 17:21:58 -0300

    add Todo model

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,25 @@
+(function (window){
+
+  var store = new Storage({
+    resource: 'todos',
+    template: '#todos'
+  });
+
+  function Todo(title, active) {
+    this.title = title || "";
+    this.active = active || false;
+
+    var dataTodo = {
+      'todo': {
+        'title': this.title,
+        'active': this.active
+      }
+    }
+
+    store.save(dataTodo);
+
+  }
+
+  window.Todo = Todo;
+
+}(this));
